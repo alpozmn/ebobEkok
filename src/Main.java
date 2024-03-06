9082a62272dbedfddbcdc3c346e662beb6cefe7b
Yunus Alp Özmen <yunus9095@gmail.com>
2024-03-06 17:58:22 +0300

    ava ile iki sayının EBOB ve EKOK değerlerini "While Döngüsü" kullanarak

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan iki sayı alınır
+        System.out.print("İki sayı girin: ");
+        int sayi1 = scanner.nextInt();
+        int sayi2 = scanner.nextInt();
+
+        // Sayıların kopyaları alınır
+        int n1 = sayi1;
+        int n2 = sayi2;
+
+        // EBOB hesaplama
+        while (sayi1 != sayi2) {
+            if (sayi1 > sayi2)
+                sayi1 -= sayi2;
+            else
+                sayi2 -= sayi1;
+        }
+        int ebob = sayi1; // veya sayi2
+
+        // EKOK hesaplama
+        int ekok = (n1 * n2) / ebob;
+
+        // Sonuçları ekrana yazdır
+        System.out.println("EBOB: " + ebob);
+        System.out.println("EKOK: " + ekok);
+    }
+}
